43bc23d1d9a7761797aef36a7ee937e29726cf1c
Dan Ott <dan@dtott.com>
2013-08-28 15:49:30 -0400

    trying to add front matter to json file

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,72 +4,6 @@ module.exports = function(grunt) {
     // Project configuration.
     grunt.initConfig({
         pkg: projectPackage,
-        watch: {
-            // scripts: {
-            //     files: ['<%= ui.ui %>/scripts/{,*/}*.js'],
-            //     tasks: ['uglify:dist']
-            // },
-            styles: {
-                files: ['<%= ui.ui %>/styles/less/{,*/}*.less'],
-                tasks: ['less:dev']
-            },
-            templates: {
-                files: ['<%= ui.ui %>/handlebars-templates/{,*/}*.hbs'],
-                tasks: ['handlebars:compile']
-            }
-        },
-        clean: {
-            dist: ['<%= ui.dist %>/**/*', '!<%= ui.dist %>/**/.svn']
-        },
-        uglify: {
-            dist: {
-                options: {
-                  mangle: false
-                },
-                files: {
-                    '<%= ui.dist %>/scripts/jakos.<%= pkg.version %>.min.js': projectPackage.globalScripts,
-                    '<%= ui.dist %>/scripts/jakos-apparel-wizard.<%= pkg.version %>.min.js': projectPackage.apparelWizardScripts
-                }
-            }
-        },
-        copy: {
-            scripts: {
-                files: [
-                {
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= ui.ui %>/scripts/thirdparty',
-                    dest: '<%= ui.dist %>/scripts/thirdparty',
-                    src: [
-                        '**/*.*',
-                        '!.svn/**/*',
-                        '!**/.svn/**/*'
-                    ]
-                },
-                {
-                    expand: true,
-                    dot: true,
-                    cwd: '<%= ui.ui %>/scripts/upload',
-                    dest: '<%= ui.dist %>/scripts/upload',
-                    src: [
-                        '**/*.*',
-                        '!.svn/**/*',
-                        '!**/.svn/**/*'
-                    ]
-                }
-                ]
-            },
-        },
-        handlebars: {
-            compile: {
-                options: {
-                    namespace: "JP.templates"
-                },
-                files: {
-                    "ui/handlebars-templates/templates.js": "ui/handlebars-templates/**/*.hbs",
-                }
-            }
-        },
         fileindex: {
             list: {
                 options: {
@@ -80,22 +14,30 @@ module.exports = function(grunt) {
                     {dest: 'imagelist.json', src: ['images/**/*'], filter: 'isFile'}
                 ]
             },
+        },
+        usebanner: {
+            taskName: {
+                options: {
+                    position: 'top',
+                    banner: '---\n'+
+                            'layout: none\n'+
+                            '---'
+                },
+                files: {
+                    src: [ 'imagelist.json' ]
+                }
+            }
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-clean');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-handlebars');
-    grunt.loadNpmTasks('grunt-contrib-compass');
-    grunt.loadNpmTasks('grunt-fileindex');
 
+    grunt.loadNpmTasks('grunt-fileindex');
+    grunt.loadNpmTasks('grunt-banner');
 
     // Default task(s).
 
 
-    grunt.registerTask('build', ['fileindex']);
+    grunt.registerTask('build', ['fileindex', 'usebanner']);
 
 
 
